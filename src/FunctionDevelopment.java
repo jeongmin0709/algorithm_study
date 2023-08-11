9e08ddaeb3d422176144c8b94cac834d4fac1e4e
Jeong min <tjrwjdals7@naver.com>
2023-08-11 17:50:30 +0900

    <type>: 프로그래머스 레벨2 기능개발
    <body>
    
    <footer>

---
+++ src/FunctionDevelopment.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class FunctionDevelopment {
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
+
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
